c51ba297da958a0f0b615d04d122a8c0b8c5a59c
Saifee Rahman Saif <saifeer1019@gmail.com>
2024-12-03 00:21:10 +0600

    edits

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,5 +1,9 @@
 import { NextResponse } from 'next/server';
+import dbConnect from '@/app/lib/dbconnect';
+import User from '@/models/User';
 
 export async function GET(request) {
-  return NextResponse.json({ message: 'Hello, world!' });
+  await dbConnect();
+  const users = await User.find();
+  return NextResponse.json({ 'users': users });
 }
